c813c630f220260db13035030ae2d8a0e05218c4
Deborahcsm12 <deborahmawi12@gmail.com>
2026-03-08 23:28:15 -0700

    Final Draft

---
+++ lib/student.dart
@@ -0,0 +1,13 @@
+class Student {
+  final String name;
+  final String major;
+  final DateTime enrollmentDate;
+  final String about;
+
+  Student({
+    required this.name,
+    required this.major,
+    required this.enrollmentDate,
+    required this.about,
+  });
+}
